3d92abaebdd91c72161646b89938f586896bbe75
Вадим <vadimmm.m@ya.ru>
2020-10-04 12:18:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Pr4S1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList MyList = new ArrayList();
+            MyList.Add("First");
+            MyList.Add("Second");
+            MyList.Add("Third");
+            MyList.Add("Fourth");
+
+            foreach (string item in MyList)
+            {
+                Console.WriteLine("Unsorted: {0}", item);
+            }
+
+            MyList.Sort();
+
+            foreach (string item in MyList)
+            {
+                Console.WriteLine("Sorted: {0}", item);
+            }
+        }
+    }
+}
